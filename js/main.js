a5f03412c2088f6f40da40787d9b1001c5f07ca6
Elena Grischenko <elenaveselovskaya2017@gmail.com>
2023-09-21 16:55:42 +0300

    Задание блок 1.5

---
+++ js/main.js
@@ -0,0 +1,52 @@
+let servicesContentBtn = document.querySelector('.services-content__btn');
+let servicesContentBtnImg = document.querySelector('.services-content__btn-img');
+let servicesContentBrandWrapperHeight = document.querySelector('.services-content-slider__wrapper');
+
+servicesContentBtn.addEventListener('click', function() {
+    
+
+    if (servicesContentBtn.textContent !== 'Показать все') {
+        servicesContentBtn.textContent = 'Показать все';
+        servicesContentBtnImg.style.transform = 'rotate(0deg)';
+
+        if (window.innerWidth >= 768) {
+            servicesContentBrandWrapperHeight.style.height = '160px';
+        } 
+        else {
+            servicesContentBrandWrapperHeight.style.height = '100%';
+    }
+        
+         
+    }
+    else { 
+        servicesContentBrandWrapperHeight.style.height = '100%';
+        servicesContentBtn.textContent = 'Скрыть';
+        servicesContentBtnImg.style.transform = 'rotate(180deg)';
+         
+    }  
+})
+
+
+var init = false;
+var swiper;
+function swiperCard() {
+  if (window.innerWidth <= 768) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper-container", {
+        direction: "horizontal",
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+swiperCard();
+window.addEventListener("resize", swiperCard);
+          
